c36b568fbaa6ffa95079cb2f06cf64f393f26ad8
lochana2002 <ehltehelapitiya@gmail.com>
2025-09-17 19:38:31 +0530

    feat:GetAuctionById created.

--- AuctionManagementSystem/Program.cs
+++ AuctionManagementSystem/Program.cs
@@ -1,16 +1,22 @@
 using AuctionManagementSystem.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization; // Make sure to add this using directive
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+  .AddJsonOptions(options =>
+  {
+      options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+  });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // EF Core with SQL Server Express
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
